59762eabc9a21446b5ef83dfebab9ed9fab816e6
John Newman <john@beeper.com>
2023-09-12 15:22:36 +0100

    add server cancellation to interface

--- interface.go
+++ interface.go
@@ -9,6 +9,7 @@ type RobotClient interface {
 	ServerGetList() ([]models.Server, error)
 	ServerGet(ip string) (*models.Server, error)
 	ServerSetName(ip string, input *models.ServerSetNameInput) (*models.Server, error)
+	ServerSetCancellation(ip string, input *models.CancellationSetInput) (*models.Cancellation, error)
 	ServerReverse(ip string) (*models.Cancellation, error)
 	KeyGetList() ([]models.Key, error)
 	IPGetList() ([]models.IP, error)
