d4ebfabc1835c32b164a155f46a387de924678ee
the-newman <john@beeper.com>
2023-09-13 00:16:40 +0100

    Update cancellation api (#2)
    * make cancellation reason optional
    
    * add get server cancellation

--- server.go
+++ server.go
@@ -46,12 +46,30 @@ func (c *Client) ServerGet(ip string) (*models.Server, error) {
 	return &serverResp.Server, nil
 }
 
+func (c *Client) ServerGetCancellation(ip string) (*models.Cancellation, error) {
+	url := fmt.Sprintf(c.baseURL+"/server/%s/cancellation", ip)
+
+	bytes, err := c.doGetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var cancelResp models.CancellationResponse
+	err = json.Unmarshal(bytes, &cancelResp)
+	if err != nil {
+		return nil, err
+	}
+	return &cancelResp.Cancellation, nil
+}
+
 func (c *Client) ServerSetCancellation(ip string, input *models.CancellationSetInput) (*models.Cancellation, error) {
 	url := fmt.Sprintf(c.baseURL+"/server/%s/cancellation", ip)
 
 	formData := neturl.Values{}
+	if input.CancellationReason != "" {
+		formData.Set("cancellation_reason", input.CancellationReason)
+	}
 	formData.Set("cancellation_date", input.CancellationDate)
-	formData.Set("cancellation_reason", input.CancellationReason)
 	formData.Set("reserve_location", strconv.FormatBool(input.ReserveLocation))
 
 	bytes, err := c.doPostFormRequest(url, formData)
